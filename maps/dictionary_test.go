af74871dc8a376d445bc6e3ac85e0e63dc87d7a9
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-07 17:48:05 +0300

    feat: map04

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func TestSearch(t *testing.T) {
