79459ebba0927633def544568dec27e6581cc452
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-07 17:02:54 +0300

    feat: return error in dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,13 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find a word you were looking for")
+	}
+	return definition, nil
 }
