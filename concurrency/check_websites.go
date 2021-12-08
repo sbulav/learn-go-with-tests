b0a9bd522bf7a1af73ee13744b5249769de0d318
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-08 16:47:01 +0300

    feat: concurrency3, add goroutines

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,21 +1,29 @@
 package concurrency
 
-import "time"
-
 // WebsiteChecker checks a url, returning a bool.
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites takes a WebsiteChecker and a slice of urls and returns  a map.
 // of urls to the result of checking each url with the WebsiteChecker function.
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChan := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChan <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChan
+		results[r.string] = r.bool
+	}
 
 	return results
 }
