1999edd34f5fc6ea06b4e2b64f2e52fd19a2a09c
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-08 16:36:42 +0300

    feat: concurrency2

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,5 +1,7 @@
 package concurrency
 
+import "time"
+
 // WebsiteChecker checks a url, returning a bool.
 type WebsiteChecker func(string) bool
 
@@ -9,8 +11,11 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
+	time.Sleep(2 * time.Second)
 
 	return results
 }
