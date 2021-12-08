e57ab8e6e929b8ffe18e05903b7b9489669e3bd8
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-08 15:19:15 +0300

    feat: mock 4, SpyCountdownOperations

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -7,33 +7,30 @@ import (
 	"time"
 )
 
-const (
-	finalWord      = "Go!"
-	countdownStart = 3
-)
-
+// Sleeper allows you to put delays.
 type Sleeper interface {
 	Sleep()
 }
+
+// DefaultSleeper is an implementation of Sleeper with a predefined delay.
 type DefaultSleeper struct{}
 
-type SpySleeper struct {
-	Calls int
-}
-
+// Sleep will pause execution for the defined Duration.
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
+const finalWord = "Go!"
+const countdownStart = 3
 
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper.
 func Countdown(out io.Writer, sleeper Sleeper) {
+
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
