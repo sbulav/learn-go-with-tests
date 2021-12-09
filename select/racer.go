f70a5ce294b96a544c2b45f4de07d556d616da95
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 12:45:40 +0300

    feat: add select1

---
+++ select/racer.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+import "net/http"
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
---
+++ select/racer_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+import "net/http"
+import "net/http/httptest"
+import "time"
+
+func TestRacer(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
