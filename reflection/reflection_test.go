9ed792c4efb63ce0eaacf7b7b287edab75ad67a1
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 14:17:31 +0300

    feat: reflect2

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -21,5 +21,7 @@ func TestWalk(t *testing.T) {
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, got %d, want %d", len(got), 1)
 	}
-
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
 }
