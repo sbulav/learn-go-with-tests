220e0f8075a1b623c6869f88dc65831d5d6acf1b
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 13:29:15 +0300

    feat: select3 use ConfigurableRacer

--- select/racer.go
+++ select/racer.go
@@ -6,14 +6,20 @@ import (
 	"time"
 )
 
+var tenSecondsTimeout = time.Second * 10
+
 func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondsTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(a):
 		return a, nil
 
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -1,24 +1,45 @@
 package main
 
-import "testing"
-import "net/http"
-import "net/http/httptest"
-import "time"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
 
-	t.Run("return an error if server does not respond in 10s", func(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-		defer serverA.Close()
-		defer serverB.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
-			t.Errorf("expected an errror, but did not get one")
+			t.Error("expected an error but didn't get one")
 		}
 	})
 }
