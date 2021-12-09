2b3c6e35f65329ca295e4a394afa1b60edd1d1d6
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 14:46:36 +0300

    feat: reflection5

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,15 +5,17 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
 		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
