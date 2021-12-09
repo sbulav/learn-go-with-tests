3c182c583fa5f52bf6d7142459f7e37e32b90b7d
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 14:22:19 +0300

    refactor: reflection2

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	// "fmt"
+	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	// x = {Chris}
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d, want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
 	}
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
