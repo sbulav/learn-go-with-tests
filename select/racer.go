f60d3971f0e5600e8e1392eb571a927672a4718f
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 13:06:48 +0300

    feat: select2 use channels

--- select/racer.go
+++ select/racer.go
@@ -1,19 +1,23 @@
 package main
 
-import "time"
 import "net/http"
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+
+	case <-ping(b):
+		return b
 	}
-	return b
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+
 }
