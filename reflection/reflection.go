ec8dfb1bd6a6dc607fe1595139ada448695c0a83
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-09 14:38:04 +0300

    feat: reflect3

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -4,8 +4,16 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
 	}
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -6,6 +6,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -26,6 +36,22 @@ func TestWalk(t *testing.T) {
 				Name string
 				City string
 			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
 			[]string{"Chris", "London"},
 		},
 	}
