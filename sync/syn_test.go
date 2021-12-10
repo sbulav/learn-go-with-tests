19d941e363982c08e705e4e4c1a66e21f4c8f6c3
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-10 16:03:29 +0300

    feat: sync2

--- sync/syn_test.go
+++ sync/syn_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -13,6 +14,22 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it run safely concurrency", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
