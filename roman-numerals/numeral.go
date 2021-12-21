a0692ec972461ea3fbbce70cdf2acb4fa032d1fc
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-21 13:32:06 +0300

    feat: roman-numerals6

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -4,17 +4,25 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for i := arabic; i > 0; i-- {
-		if i == 5 {
+	for arabic > 0 {
+		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
 			result.WriteString("V")
-			break
-		}
-		if i == 4 {
+			arabic -= 5
+		case arabic > 3:
 			result.WriteString("IV")
-			break
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic -= 1
 		}
-
-		result.WriteString("I")
 	}
+
 	return result.String()
 }
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -13,6 +13,15 @@ func TestRomanNumerals(t *testing.T) {
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV", 4, "IV"},
 		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"7 gets converted to VII", 7, "VII"},
+		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
