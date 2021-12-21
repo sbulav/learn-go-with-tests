02001ffd7eb9f2985ffe4f73f1ae33d152545815
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-21 13:24:28 +0300

    feat: roman-numerals5

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -4,7 +4,16 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+
 		result.WriteString("I")
 	}
 	return result.String()
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -11,6 +11,8 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
 
 	for _, test := range cases {
