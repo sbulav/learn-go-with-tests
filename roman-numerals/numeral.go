004dc69fb92492331247ba560a428fd6244627fa
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-21 13:04:34 +0300

    feat: roman-numerals2

---
+++ roman-numerals/numeral.go
@@ -0,0 +1,8 @@
+package numerals
+
+func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
+	return "I"
+}
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,9 +3,18 @@ package numerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
