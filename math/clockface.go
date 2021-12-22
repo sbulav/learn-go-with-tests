e3ba2a2eb84cdc18be7d39b1ee73153a9b176f6c
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-22 17:21:52 +0300

    feat: math4

--- math/clockface.go
+++ math/clockface.go
@@ -5,6 +5,19 @@ import (
 	"time"
 )
 
+// A Point represents a two dimensional Cartesian coordinate.
+type Point struct {
+	X float64
+	Y float64
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
+}
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
 }
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -26,11 +26,38 @@ func TestSecondsInRadians(t *testing.T) {
 		})
 	}
 }
-
 func simpleTime(hours, minutes, seconds int) time.Time {
 	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
 
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
+}
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) &&
+		roughlyEqualFloat64(a.Y, b.Y)
 }
