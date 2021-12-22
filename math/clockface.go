a0d1ad259ccedee277f25cd7d470343173d5240c
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-22 16:00:57 +0300

    feat: math2

--- math/clockface.go
+++ math/clockface.go
@@ -1,15 +1,10 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
-// A Point represents a two dimensional Cartesian coordinate
-type Point struct {
-	X float64
-	Y float64
-}
-
-// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
-// represented as a Point.
-func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
 }
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -1,19 +1,17 @@
-package clockface_test
+package clockface
 
 import (
-	"clockface"
+	"math"
 	"testing"
 	"time"
-	// "github.com/quii/learn-go-with-tests/math/v1/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 }
