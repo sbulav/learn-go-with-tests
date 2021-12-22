b76ab372a58760c2ce51f9059b1978017dbb5af5
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-12-22 17:24:49 +0300

    feat: math5

--- math/clockface.go
+++ math/clockface.go
@@ -21,3 +21,17 @@ func secondHandPoint(t time.Time) Point {
 
 	return Point{x, y}
 }
+
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY} //translate
+	return p
+}
